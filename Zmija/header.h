284a92a616381e1cc5ca3a576b7b83933e90cee6
Ivana Zdelar <ivanazdelar22@gmail.com>
2025-05-12 18:25:52 +0200

    Update header.h

--- Zmija/header.h
+++ Zmija/header.h
@@ -1,5 +1,50 @@
-#define _CRT_SECURE_NO_WARNINGS
 
+#define HEADER_H
 
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <ctype.h>
+#include <windows.h>
+#include <process.h>
+
+#define UP 72
+#define DOWN 80
+#define LEFT 75
+#define RIGHT 77
+
+int length;
+int bend_no;
+int len;
+char key;
+void record();
+
+int life;
+void Delay(long double);
+void Move();
+void Food();
+int Score();
+void Print();
+void gotoxy(int x, int y);
+void GotoXY(int x, int y);
+void Bend();
+void Boarder();
+void Down();
+void Left();
+void Up();
+void Right();
+void ExitGame();
+int Scoreonly();
+
+struct coordinate {
+	int x;
+	int y;
+	int direction;
+};
+
+typedef  struct coordinate coordinate;
+
+coordinate head, bend[500], food, body[30];
 
 #pragma once
