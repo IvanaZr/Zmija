26583ee5927f6584c15ad5deaca5d01bf0577837
Ivana Zdelar <ivanazdelar22@gmail.com>
2025-06-03 15:10:26 +0200

    Update player.h

--- Zmija/player.h
+++ Zmija/player.h
@@ -1,30 +1,30 @@
 #define _CRT_SECURE_NO_WARNINGS
 
-// player.h
-
-#ifndef PLAYER_H
-#define PLAYER_H
-
 #include <time.h>
+
+typedef enum {
+	MENU_ADD = 1,
+	MENU_DISPLAY,
+	MENU_UPDATE,
+	MENU_DELETE,
+	MENU_SORTED,
+	MENU_SEARCH_BY_NICKNAME,
+	MENU_EXIT
+} menu_choice_t;
 
 typedef struct {
     char name[50];
     int score;
-    time_t timestamp; // vrijeme kad je rezultat postignut
+    time_t timestamp; 
 } Player;
 
 void addPlayer(const char* filename);
 void displayPlayers(const char* filename);
-
-#endif
-
-
-// player.h – dodaj ove prototipove
-
 void updatePlayer(const char* filename);
 void deletePlayer(const char* filename);
-
 int compareScoreDescending(const void* a, const void* b);
 int compareName(const void* a, const void* b);
 void displaySortedPlayers(const char* filename);
-void searchPlayerByName(const char* filename);
+void searchPlayerByNickname(const char* filename);
+
+#pragma once
